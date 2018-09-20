e8eee2ea3ef450a8d74016b51cc42621a7369cfa
Ran Har-Shuv <ran.harshuv@gmail.com>
2018-09-20 10:05:38 +0300

    some adjustments

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const path = require('path');
 
 // If an incoming request uses
 // a protocol other than HTTPS,
@@ -22,15 +22,16 @@ const forceSSL = function() {
 // middleware
 app.use(forceSSL());
 
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
-
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default
 // Heroku port
