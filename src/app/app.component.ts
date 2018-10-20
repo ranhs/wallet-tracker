f1e2ec102d95d868ae564ec16a508f1429dea2c8
ShacharHarshuv <shachar.harshuv@gmail.com>
2018-10-20 19:38:52 +0300

    wallet-action and wallet table components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
   public valueChange : number = 12.3;
+
+  constructor(){}
+  
+  ngOnInit(){
+    //document ready function
+    $(function(){
+      AppComponent.scrollContentToBottom(); //initialize the scrolling to bottom
+    });
+  }
+
+  //this function needs to be called whenever we add an item to the list
+  static scrollContentToBottom(){
+    let $content = $("#content");
+    $content.scrollTop($content.height());
+    console.log($content.scrollTop());
+    
+  }
+
+
+
 }
--- src/app/wallet-table/wallet-table.component.ts
+++ src/app/wallet-table/wallet-table.component.ts
@@ -7,9 +7,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletTableComponent implements OnInit {
 
-  constructor() { }
+  private transactions: Transaction[]; 
+
+  constructor() {
+    this.transactions=
+    [
+      {
+        title: "יתרה קודמת",
+        date: "31 אוגוסט",
+        sum: 184,
+        left: 184
+      },
+      {
+        title: "לגנדה גלי",
+        date: "1 ספטמבר",
+        sum: -28,
+        left: 156
+      },
+      {
+        title: "צהריים גלי",
+        date: "1 ספטמבר",
+        sum: -50,
+        left: 106
+      },
+      {
+        title: "חופשי יומי שחר",
+        date: "2 ספטמבר",
+        sum: -40,
+        left: 66
+      },
+      {
+        title: "משיכה",
+        date: "4 ספטמבר",
+        sum: 200,
+        left: 266
+      },
+      {
+        title: "מכונת חטיפים",
+        date: "4 ספטמבר",
+        sum: -4.5,
+        left: 261.5
+      },
+      {
+        title: "פתוח קול שחר",
+        date: "4 ספטמבר",
+        sum: -200,
+        left: 61.5
+      },
+      {
+        title: "משיכה",
+        date: "4 ספטמבר",
+        sum: 200,
+        left: 261.5
+      },
+    ]
+   }
 
   ngOnInit() {
   }
 
+}
+
+interface Transaction {
+  title: string;
+  date: string;
+  sum: number;
+  left: number;
 }
