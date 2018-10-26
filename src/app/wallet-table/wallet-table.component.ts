a2454569ee7dce996cd950a7c25cd25f6e39f399
Ran Har-Shuv <ran.harshuv@gmail.com>
2018-10-26 16:03:51 +0300

    support getting the trasactions (table) form code

--- src/app/wallet-table/wallet-table.component.ts
+++ src/app/wallet-table/wallet-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WalletTransaction } from '../wallet.transaction';
 
 @Component({
   selector: 'app-wallet-table',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletTableComponent implements OnInit {
 
-  constructor() { }
+  private rows : WalletTransaction[] = [];
+
+  constructor() {
+   }
 
   ngOnInit() {
+    // TODO: read the data from the database
+    this.rows = [];
+    this.rows.push(new WalletTransaction(new Date(2018,8,31), "יתרה קודמת", 184.2, 184.2));
+    this.rows.push(new WalletTransaction(new Date(2018,9,1), "לגנדה גלי", -28, 156.2));
+    this.rows.push(new WalletTransaction(new Date(2018,9,2), "צהריים גלי", -50, 106.2));
   }
 
 }
---
+++ src/app/wallet.transaction.ts
@@ -0,0 +1,40 @@
+export class WalletTransaction {
+    constructor( 
+        private _date : Date,
+        private _description : string,
+        private _value : number, 
+        private _total : number) {
+    }
+
+    public get date() : Date {
+        return this._date;
+    }
+
+    private dd(val: number) : string {
+        return `${(val<10)?'0':''}${val}`;
+    }
+    
+    private date2string(date: Date) : string {
+        return `${this.dd(date.getDate())}/${this.dd(date.getMonth()+1)}/${date.getFullYear()}`;
+    }
+
+    public get displayDate() : string {
+        return `${this.date2string(this._date)}`
+    }
+
+    public get description() : string {
+        return this._description;
+    }
+
+    public get value() : number {
+        return this._value;
+    }
+
+    public get displayValue() : string {
+        return `${(this._value>0)?'+':''}${this._value}`;
+    }
+
+    public get total() : number {
+        return this._total;
+    }
+}
