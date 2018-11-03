057a7b6bf68b356610c2f305d6b66ceb4230e22f
Ran Har-Shuv <ran.harshuv@gmail.com>
2018-11-03 13:14:06 +0200

    read transactions from backedit

--- src/app/transaction-storage.service.ts
+++ src/app/transaction-storage.service.ts
@@ -9,20 +9,13 @@ export class TransactionStorageService {
 
   getTransactions(startDate : Date, endDate : Date) : Promise<WalletTransaction[]> {
     return new Promise<WalletTransaction[]>((resolve, reject) => {
-      var rv = this.http.get('/transactions').subscribe( (data) => {
-        console.log('data=', data);
+      var rv = this.http.get('/transactions').subscribe( (data : {id:number, date: Date, description: string, value: number, total: number}[] ) => {
+        var transactions : WalletTransaction[] = [];
+        for ( var trans of data ) {
+          transactions.push( new WalletTransaction(trans.id, trans.date, trans.description, trans.value, trans.total));
+        }
+        resolve(transactions);
       });
-      console.log('get returned', rv);
-      var dummyTransactions : WalletTransaction[] = [];
-      dummyTransactions.push(new WalletTransaction(1, new Date(2018,8-1,31), "יתרה קודמת", 184.2, 184.2));
-      dummyTransactions.push(new WalletTransaction(2, new Date(2018,9-1,1), "לגנדה גלי", -28, 156.2));
-      dummyTransactions.push(new WalletTransaction(3, new Date(2018,9-1,2), "צהריים גלי", -50, 106.2));
-      dummyTransactions.push(new WalletTransaction(4, new Date(2018,9-1,2), "חופשי יומי שחר", -40, 66.2));
-      dummyTransactions.push(new WalletTransaction(5, new Date(2018,9-1,4), "משיכה", 200, 266.2));
-      dummyTransactions.push(new WalletTransaction(6, new Date(2018,9-1,4), "מכונת חטיפים", -4.5, 261.7));
-      dummyTransactions.push(new WalletTransaction(7, new Date(2018,9-1,4), "פתוח קול שחר", -200, 61.7));
-      dummyTransactions.push(new WalletTransaction(8, new Date(2018,9-1,8), "משיכה", 200, 261.7));
-      resolve(dummyTransactions);
     });
   }
 
