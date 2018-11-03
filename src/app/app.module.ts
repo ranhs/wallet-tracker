d56a3f0962858b927224e1293c8bb2dbba02ede0
Ran Har-Shuv <ran.harshuv@gmail.com>
2018-11-03 22:53:07 +0200

    restore HttpClientModule in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { WalletActionComponent } from './walletAction/wallet-action.component';
@@ -21,6 +22,7 @@ import { TransactionStorageService } from './transaction-storage.service';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [TransactionStorageService],
