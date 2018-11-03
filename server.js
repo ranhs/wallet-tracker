8dfbec3a24aaffb0ee9d6b3866a79b6fb1e895a6
Ran Har-Shuv <ran.harshuv@gmail.com>
2018-11-03 12:08:34 +0200

    cancel the delay on publish

--- server.js
+++ server.js
@@ -1,44 +1,40 @@
 const path = require('path');
 const express = require('express');
+const plugins = require('./src/app/server.plugins');
 
-setTimeout( () => {
-  const plugins = require('./src/app/server.plugins');
-  var app = express();
-  // If an incoming request uses
-  // a protocol other than HTTPS,
-  // redirect that request to the
-  // same url but with HTTPS
-  const forceSSL = function() {
-    return function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-        );
-      }
-      next();
+var app = express();
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+      ['https://', req.get('Host'), req.url].join('')
+      );
     }
+    next();
   }
+}
 
-  // Instruct the app
-  // to use the forceSSL
-  // middleware
-  app.use(forceSSL());
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
 
-  // Run the app by serving the static files
-  // in the dist directory
-  app.use(express.static(__dirname + '/dist'));
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-  //plugins.plugin(app);
+//plugins.plugin(app);
 
-  // For all GET requests, send back index.html
-  // so that PathLocationStrategy can be used
-  app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-  });
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
-  // Start the app by listening on the default
-  // Heroku port
-  app.listen(process.env.PORT || 8080);
-
-}, 500);
-
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
