f1dc4c471318ea73612f01598428ad7effd6d76b
Ran Har-Shuv <ran.harshuv@gmail.com>
2018-11-03 12:43:42 +0200

    return json for transactions

--- src/app/server.plugins.js
+++ src/app/server.plugins.js
@@ -1,8 +1,14 @@
 "use strict";
 exports.__esModule = true;
+var dummyTransactions = [
+    { id: 1, date: new Date(2018, 8 - 1, 31), description: "יתרה קודמת", value: 184.2, total: 184.2 }, ,
+    { id: 2, date: new Date(2018, 9 - 1, 1), description: "לגנדה גלי", value: -28, total: 156.2 },
+    { id: 3, date: new Date(2018, 9 - 1, 2), description: "צהריים גלי", value: -50, total: 106.2 },
+    { id: 4, date: new Date(2018, 9 - 1, 2), description: "חופשי יומי שחר", value: -40, total: 66.2 }
+];
 function plugin(app) {
     app.get('/transactions', function (req, res) {
-        res.send('Hello World!');
+        res.json(dummyTransactions);
     });
 }
 exports.plugin = plugin;
--- src/app/server.plugins.ts
+++ src/app/server.plugins.ts
@@ -1,5 +1,12 @@
+const dummyTransactions : {id: number, date: Date, description: string, value: number, total: number }[]= [
+    { id: 1, date: new Date(2018,8-1,31), description: "יתרה קודמת", value: 184.2, total: 184.2 },,
+    { id: 2, date: new Date(2018,9-1,1), description: "לגנדה גלי", value: -28, total: 156.2 },
+    { id: 3, date: new Date(2018,9-1,2), description: "צהריים גלי", value: -50, total: 106.2},
+    { id: 4, date: new Date(2018,9-1,2), description: "חופשי יומי שחר", value: -40, total: 66.2}
+];
+
 export function plugin(app) {
     app.get('/transactions', (req, res) => {
-        res.send('Hello World!');
+        res.json(dummyTransactions);
     });
 }
